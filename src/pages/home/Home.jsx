82c1af867ad9f18fd4ca15573189d9eefd255ef3
everton-lacerda <everton_ml@outlook.com>
2022-03-16 23:40:40 -0300

    start project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./home.scss";
+const Home = () => {
+  return <div className="home">
+      <h1 className="title">home</h1>
+  </div>;
+};
+
+export default Home;
