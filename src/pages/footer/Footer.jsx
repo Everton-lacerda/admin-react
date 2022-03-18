9844beb9a50319c19cd8d9f00cd6f0bfdb1a1c2e
everton-lacerda <everton_ml@outlook.com>
2022-03-18 10:11:52 -0300

    make it better

---
+++ src/pages/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.scss'
+const Footer = () => {
+  return (
+    <footer>
+      <div class="footer-content"> 
+        <p>© 2022 Destro Tecnologia. Todos os direitos reservados.</p>
+        <p>Feito com ❤️ por X-Side</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,96 @@
 import React from "react";
 import "./home.scss";
+
+import card1 from "../../assets/PNG/Scenes/1x/Shopping, e-commerce _ purchase, shopping, shop, commerce, payment, store.png";
+import card2 from "../../assets/PNG/Scenes/1x/Business, Startup, Growth, success _ build, idea, innovation, man, thought, grow, smart.png";
+import card3 from "../../assets/PNG/Scenes/1x/Shopping, e-commerce, finance _ store, online, shop, woman, invoice, fashion, purchase.png";
+import arrowLeft from "../../assets/next.png";
+import Footer from "../footer/Footer";
 const Home = () => {
-  return <div className="home">
-      <h1 className="title">home</h1>
-  </div>;
+  return (
+    <div>
+      <div className="header">
+        <h1 className="title">Sistema de compras</h1>
+        <h2 className="subTitle">
+          <strong>DestroTech</strong> - Cotação online
+        </h2>
+      </div>
+      <div className="container">
+        <article className="card card1">
+          <a href="#" className="card__link">
+            <div className="card__icon">
+              <img src={card1} alt="Logo" width={150} />
+            </div>
+
+            <div className="card__media">
+              <img src={card1} alt="Logo" width={200} />
+            </div>
+
+            <div className="card__header">
+              <h3 className="card__header-title">Processo de compras</h3>
+              <p className="card__header-meta">
+                <ul>
+                  <li>Consolidação das solicitações</li>
+                  <li>Definição de requisição</li>
+                  <li>Pedido do direto e cotação online</li>
+                </ul>
+              </p>
+              <div className="card__header-icon">
+                <img src={arrowLeft} alt="Logo" width={40} />
+              </div>
+            </div>
+          </a>
+        </article>
+        <article className="card card2">
+          <a href="#" className="card__link">
+            <div className="card__icon">
+              <img src={card2} alt="Logo" width={150} />
+            </div>
+            <div className="card__media">
+              <img src={card2} alt="Logo" width={150} />
+            </div>
+
+            <div className="card__header">
+              <h3 className="card__header-title">Fornecedores</h3>
+              <p className="card__header-meta">
+                <ul>
+                  <li>Cotação online Uso exclusivo do fornecedor</li>
+                </ul>
+              </p>
+              <div className="card__header-icon">
+                <img src={arrowLeft} alt="Logo" width={40} />
+              </div>
+            </div>
+          </a>
+        </article>
+        <article className="card card3">
+          <a href="#" className="card__link">
+            <div className="card__icon">
+              <img src={card3} alt="Logo" width={150} />
+            </div>
+
+            <div className="card__media">
+              <img src={card3} alt="Logo" width={150} />
+            </div>
+
+            <div className="card__header">
+              <h3 className="card__header-title ">compras online</h3>
+              <p className="card__header-meta">
+                <ul>
+                  <li>Consolidação e apuração das <br /> cotações</li>
+                  <li>Emissão de pedido online</li>
+                </ul>
+              </p>
+              <div className="card__header-icon">
+                <img src={arrowLeft} alt="Logo" width={40} />
+              </div>
+            </div>
+          </a>
+        </article>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
